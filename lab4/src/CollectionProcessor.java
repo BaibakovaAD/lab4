3b5913d3d7e739c00a9f787decce3cd1dbfe82ea
BaibakovaAD <nastya.bay.ry@gmail.com>
2024-11-21 08:32:01 +0500

    Update CollectionProcessor.java

--- lab4/src/CollectionProcessor.java
+++ lab4/src/CollectionProcessor.java
@@ -1,5 +1,10 @@
 import java.util.*;
 import java.util.function.*;
-public interface CollectionProcessor<T, P extends Collection<T>> {
-    P process(List<T> sourceList, Supplier<P> collectionFactory, BiConsumer<T, P> processingLogic);
+
+public interface CollectionProcessor<T, P extends Collection> {
+    P process(
+            List<T> sourceList,
+            Supplier<P> collectionFactory,
+            BiConsumer<T, P> processingLogic
+    );
 }
